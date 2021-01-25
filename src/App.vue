526a0d8bf9096d0bfd90774c52214744d196798d
hakzzang <mbh930312@naver.com>
2021-01-25 18:20:19 +0900

    2021.01.25.클론코딩 진행

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <l-n-b>d</l-n-b>
+    <my-header>
+    </my-header>
+  </div>
+</template>
+
+<script>
+import LNB from '~/src/components/LNB'
+import MyHeader from "~/src/components/MyHeader"
+
+export default {
+  components: {
+    LNB,
+    MyHeader
+  }
+}
+
+</script>
+
+<style lang="scss">
+  @import "./scss/main.scss";
+
+  .container {
+    display: flex;
+    .item {
+      width: 100px;
+      height: 100px;
+      background-color:royalblue;
+    }
+  }
+</style>
