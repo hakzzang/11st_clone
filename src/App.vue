06f668c19193c0a6e826bffe5c9ac623bd7f70d1
hakzzang <mbh930312@naver.com>
2021-01-27 14:56:14 +0900

    feat: GlobalBanner 레이아웃 반영

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     <billboard></billboard>
     <direct></direct>
     <hot-focus></hot-focus>
+    <global-banner></global-banner>
   </div>
 </template>
 
@@ -15,14 +16,15 @@ import MyHeader from "~/src/components/MyHeader"
 import Billboard from "~/src/components/Billboard";
 import Direct from "~/src/components/Direct";
 import HotFocus from "~/src/components/HotFocus";
-
+import GlobalBanner from "~/src/components/GlobalBanner";
 export default {
   components: {
     LNB,
     MyHeader,
     Billboard,
     Direct,
-    HotFocus
+    HotFocus,
+    GlobalBanner
   }
 }
 
