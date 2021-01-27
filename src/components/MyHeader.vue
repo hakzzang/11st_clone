01373beb858fb5a7db7d36bcd9613bbac28bc5db
hakzzang <mbh930312@naver.com>
2021-01-27 12:03:05 +0900

    feat: MyHeader에 상단 배너 기능 추가

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -109,6 +109,34 @@
         </ul>
       </div>
     </header>
+    <div
+      :class="{ fixed : isFixed }"
+      class="utils">
+      <div class="inner">
+        <ul>
+          <li>
+            <a href="javascript:void(0)">베스트</a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">쿠폰/혜택</a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">기획전</a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">오늘장보기</a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">T공식대리점</a>
+          </li>
+          <li>
+            <a
+              class="shocking-deal"
+              href="javascript:void(0)"></a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -116,7 +144,7 @@
 import Swiper from 'swiper'
 import 'swiper/swiper-bundle.css'
 import dayjs from 'dayjs'
-
+import _throttle from 'lodash/throttle'
 
 export default {
   data() {
@@ -131,7 +159,8 @@ export default {
         {name: '회원정보', href: 'javascript:void(0)'}
       ],
       isShowRankingWrap: false,
-      tabIndex: 0
+      tabIndex: 0,
+      isFixed: false
     }
   }, mounted() {
     this.init()
@@ -151,6 +180,9 @@ export default {
   },
   methods: {
     async init() {
+      window.addEventListener('scroll', _throttle(event => {
+        this.isFixed = window.scrollY > 120
+      }, 100))
       this.rankings = await this.$fetch({
         requestName: 'rankings'
       })
@@ -525,6 +557,52 @@ header {
               }
             }
           }
+        }
+      }
+    }
+  }
+}
+
+.utils {
+  border-top: 1px solid #f1f1f1;
+  &.fixed {
+    padding-top: 120px;
+  }
+  .inner {
+    width: 1240px;
+    margin: 0 auto;
+  }
+  ul {
+    display: flex;
+    li {
+      margin-right: 25px;
+      &:last-child {
+        margin-right: 0;
+      }
+      a {
+        display: flex;
+        align-items: center;
+        height: 66px;
+        font-size: 17px;
+        position: relative;
+        &::after {
+          content: "";
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          height: 2px;
+          background-color: #f43142;
+          opacity: 0;
+        }
+        &:hover::after {
+          opacity: 1;
+        }
+        &.shocking-deal {
+          width: 63px;
+          background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+          background-position: -94px 0;
+          background-size: 363px;
         }
       }
     }
